8bc3e320955b3498ac3fc6236bcb07cf96338338
NguyenLoc99 <locnguyen@playgroundvina.com>
2023-09-14 14:19:35 +0700

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -21,6 +21,8 @@ const Home = () => {
   } = useSwrInfiniteProducts({
     search: textSearchDebounce,
   });
+
+  
   const handleOnchangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setTextSearch(e.target.value);
   };
